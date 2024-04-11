981b104400f1d8713bc816593155176d069b50bd
ubuntu <priyanshusinghajm@gmail.com>
2024-04-12 00:13:02 +0530

    done with problem-4

---
+++ problem4.js
@@ -0,0 +1,12 @@
+// ==== Problem #4 ====
+// The accounting team needs all the years from every car on the lot. Execute a function that will return an array from the dealer data containing only the car years and log the result in the console as it was returned.
+
+const problem4 = (inventory) => {
+  const result = [];
+  for (let i = 0; i < inventory.length; i++) {
+    result.push(inventory[i].car_year);
+  }
+  return result;
+};
+
+module.exports = problem4;
