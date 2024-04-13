58dd44f35a6e116608e4ab2d19558b46141ef6ea
ubuntu <priyanshusinghajm@gmail.com>
2024-04-13 21:22:41 +0530

    updated the code

--- test/testProblem5.js
+++ test/testProblem5.js
@@ -2,6 +2,6 @@ const problem5 = require("../problem5");
 
 const inventory = require("../inventory");
 
-const result = problem5(inventory);
+const result = problem5(inventory, 2000);
 
 console.log(result.length);
