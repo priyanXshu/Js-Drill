c09ea4ef94662587adc0800a5e7f91bea3bc86a9
ubuntu <priyanshusinghajm@gmail.com>
2024-04-13 21:23:24 +0530

    updated the code

--- problem5.js
+++ problem5.js
@@ -1,14 +1,14 @@
 // ==== Problem #5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the array you just obtained from the previous problem, find out how many cars were made before the year 2000 and return the array of older cars and log its length.
 
-const problem5 = (inventory) => {
-    const result = []
-    for(let i=0; i<inventory.length; i++) {
-        if(inventory[i].car_year < '2000') {
-            result.push(inventory[i]);
-        }
+const problem5 = (inventory, year) => {
+  const result = [];
+  for (let i = 0; i < inventory.length; i++) {
+    if (inventory[i].car_year < year) {
+      result.push(inventory[i]);
     }
-    return result
+  }
+  return result;
 };
 
 module.exports = problem5;
